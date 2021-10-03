fe569b7b030eb533a6e815988516a111bc67a80b
GreenSulley <sukeycz@gmail.com>
2021-10-03 23:17:38 +0800

    feat: socket原生实现adb

---
+++ lib/adb_socket.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @author: Green Sulley
+# @license: MIT License
+# @project : GreenSulley/ArknightsAutoHelper
+# @file: adb_socket.py
+# @desc: Arknights Auto Helper based on ADB and Python
